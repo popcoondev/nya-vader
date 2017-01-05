2c7e9f85c7aff1982f93249e20fde850236af9a6
popcoondev <popcoondev@gmail.com>
2017-01-05 22:24:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ window.onload = function() {
 			// this.backgroundColor = 'yellow';
 			this.on('enterframe', function() {
 				if(this.age % 30 == 0) {
-					var sound = core.assets['res/bgm1.wav'].play();
+					//var sound = core.assets['res/bgm1.wav'].clone();
 					//sound.volume = 0.1;
 					//sound.play();
 				}
